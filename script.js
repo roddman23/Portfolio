f28cc8e11c64641ff982a4c4aac3bf22eccd532d
rodmen <rodrigo.gasolero@gmail.com>
2023-09-22 14:58:02 -0300

    botonsubir

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+var scrollToTopButton = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop > 300) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+
+scrollToTopButton.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
